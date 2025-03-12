8b522c149178d15037caaaeafced8b69ede7fde1
thitinan147 <thitinan147@odds.team>
2025-03-12 18:07:03 +0700

    fix bug

--- web/src/pages/IPCamera.tsx
+++ web/src/pages/IPCamera.tsx
@@ -56,10 +56,6 @@ const IPCamera = () => {
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex flex-grow items-center justify-center p-6">
-<<<<<<< Updated upstream
-                <div className="bg-white shadow-xl rounded-lg p-8 h-full flex flex-col justify-center items-center">
-                    <VideoFeed src="http://rtsp_to_web:8083/stream/27aec28e-6181-4753-9acd-0456a75f0289/channel/0/hls/live/index.m3u8" ref={videoRef} />
-=======
                 <div className="bg-white shadow-xl rounded-lg p-8 h-full flex flex-col justify-center items-center gap-6">
                     <VideoFeed
                         src="http://localhost:8083/stream/27aec28e-6181-4753-9acd-0456a75f0289/channel/0/hls/live/index.m3u8"
@@ -71,7 +67,6 @@ const IPCamera = () => {
                     >
                         Capture
                     </button>
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
