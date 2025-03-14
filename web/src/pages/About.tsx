b88a79123c68287c4754fc386fb4b37ca3b3faee
thitinan147 <thitinan147@odds.team>
2025-03-14 16:24:58 +0700

    responsive about us page

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -1,50 +1,77 @@
 import { PureComponent } from 'react'
 import ProfileCard from '../components/ProfileCard'
 
+const profiles = [
+  {
+    name: 'Suphakorn L.',
+    position: 'Back-end',
+    imageURL: 'public/Image/J.svg',
+    contacts: {
+      facebook: 'https://www.facebook.com/suphakorn.lakthong',
+      github: 'https://github.com/Suphakorn07',
+      instagram: 'https://www.instagram.com/spk.lt/'
+    }
+  },
+
+  {
+    name: 'Isoon S.',
+    position: 'IT Support',
+    imageURL: 'public/Image/ozone.svg',
+    contacts: {
+      facebook: 'https://www.facebook.com/ai.soon.2024',
+      github: 'https://github.com/IsoonSer',
+      instagram: 'https://www.instagram.com/is_beginner/'
+    }
+  },
+  {
+    name: 'Chotipat S.',
+    position: 'Back-end',
+    imageURL: 'public/Image/kul.svg',
+    contacts: {
+      facebook: 'https://www.facebook.com/ChotipatSrisawangpanyakul',
+      github: 'https://github.com/chotipats2092002',
+      instagram: 'https://www.instagram.com/chotyaccforsadnessandfailing/'
+    }
+  },
+  {
+    name: 'Thitinan P.',
+    position: 'UX/UI, Front-end',
+    imageURL: 'public/Image/tek.svg',
+    contacts: {
+      facebook: 'https://www.facebook.com/TekNimini?locale=th_TH',
+      github: 'http://github.com/thitinan147',
+      instagram: 'https://www.instagram.com/tekni____/'
+    }
+  },
+
+  {
+    name: 'Jetsadakorn P.',
+    position: 'Full-stack',
+    imageURL: 'public/Image/arm.svg',
+    contacts: {
+      facebook: 'https://www.facebook.com/profile.php?id=100006769445652',
+      github: 'https://github.com/XsoE',
+      instagram: 'https://www.instagram.com/arm_jsdk/'
+    }
+  }
+]
+
 export class About extends PureComponent {
   render() {
     return (
-      <>
-        <div className="flex flex-grow items-center justify-center p-6">
-          <div className='flex flex-col w-full gap-4'>
-            <div className='flex flex-row justify-center w-full h-full gap-4'>
+      <div className="flex flex-col flex-grow items-center justify-center p-6 w-full xl:w-[1040px] mx-auto space-y-8">
+         
 
-              <ProfileCard name={'Thitinan P.'} position={'Front-end'} imageURL={'public/Image/tek.svg'} contacts={{
-                facebook: 'https://www.facebook.com/TekNimini?locale=th_TH',
-                github: 'http://github.com/thitinan147',
-                instagram: 'https://www.instagram.com/tekni____/'
-              }} />
-              <ProfileCard name={'Isoon S.'} position={'IT Support'} imageURL={'public/Image/ozone.svg'} contacts={{
-                facebook: 'https://www.facebook.com/ai.soon.2024',
-                github: 'https://github.com/IsoonSer',
-                instagram: 'https://www.instagram.com/is_beginner/'
-              }} />
-
-              <ProfileCard name={'Chotipat S.'} position={'Back-end'} imageURL={'public/Image/kul.svg'} contacts={{
-                facebook: 'https://www.facebook.com/ChotipatSrisawangpanyakul',
-                github: 'https://github.com/chotipats2092002',
-                instagram: 'https://www.instagram.com/chotyaccforsadnessandfailing/'
-              }} />
-
-            </div>
-
-            <div className='flex flex-row justify-center w-full h-full gap-4'>
-              <ProfileCard name={'Suphakorn L.'} position={'Back-end'} imageURL={'public/Image/J.svg'} contacts={{
-                facebook: 'https://www.facebook.com/suphakorn.lakthong',
-                github: 'https://github.com/Suphakorn07',
-                instagram: 'https://www.instagram.com/spk.lt/'
-              }} />
-
-              <ProfileCard name={'Jetsadakorn P.'} position={'Full-stack'} imageURL={'public/Image/arm.svg'} contacts={{
-                facebook: 'https://www.facebook.com/profile.php?id=100006769445652',
-                github: 'https://github.com/XsoE',
-                instagram: 'https://www.instagram.com/arm_jsdk/'
-              }} />
-            </div>
-          </div>
-        </div>
-      </>
-
+        {/* Team Profiles Section */}
+        <h2 className="text-2xl font-bold text-gray-800 ">Our Team</h2>
+        <ul className="flex flex-wrap gap-4 justify-center">
+          {profiles.map((profile, index) => (
+            <li key={index}>
+              <ProfileCard {...profile} />
+            </li>
+          ))}
+        </ul>
+      </div>
     )
   }
 }
