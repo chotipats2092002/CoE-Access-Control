0ebd6051aec4e4e7af52638bb9a6b10d427055f5
thitinan147 <thitinan147@odds.team>
2025-03-15 23:43:02 +0700

    responsive x

--- web/src/pages/IPCamera.tsx
+++ web/src/pages/IPCamera.tsx
@@ -1,8 +1,8 @@
 import CameraCapture from "../components/CameraCapture";
 
 const IPCamera = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen pt-20">
+    return ( 
+        <div className="flex flex-col items-center justify-center min-h-[max(768px,100vh)] pt-20">
             <CameraCapture />
         </div>
     )
